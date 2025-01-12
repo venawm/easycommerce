22dca3a10ac855c94d2559737c1f5b084e30b8e8
venawm <pandesamir3@gmail.com>
2025-01-12 23:30:08 +0545

    feat: add dynamic metadata and folder structures

---
+++ lib/constants/index.ts
@@ -0,0 +1,5 @@
+export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || "Easycommerce";
+export const APP_DESCRIPTION =
+  process.env.APP_DESCRIPTION || "Ecommerce made easy.";
+export const SERVER_URL =
+  process.env.NEXT_PUBLIC_SERVER_URL || "http://localhost:3000";
