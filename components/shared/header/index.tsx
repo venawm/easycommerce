6fb8911b2ecbea13a311c890784f018a6907cbfd
venawm <pandesamir3@gmail.com>
2025-01-12 23:58:13 +0545

    feat: add theme toogle from next-them

--- components/shared/header/index.tsx
+++ components/shared/header/index.tsx
@@ -3,6 +3,7 @@ import Image from "next/image";
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
 import { APP_NAME } from "@/lib/constants";
+import ModeToogle from "./mode-toogle";
 
 const Header = () => {
   return (
@@ -23,13 +24,14 @@ const Header = () => {
           </Link>
         </div>
         <div className="space-x-2">
+          <ModeToogle />
           <Button asChild variant="ghost">
             <Link href="/cart">
               <ShoppingCart />
               Cart
             </Link>
           </Button>
-          <Button asChild variant="ghost">
+          <Button asChild>
             <Link href="/sign-in">
               <UserIcon />
               Sign In
