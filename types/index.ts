72f8182af9039682d6646c54023d3c5643f63879
venawm <pandesamir3@gmail.com>
2025-01-23 00:42:13 +0545

    feat(cart): add cart schema and validation

--- types/index.ts
+++ types/index.ts
@@ -1,4 +1,8 @@
-import { insertProductSchema } from "@/lib/validators";
+import {
+  insertProductSchema,
+  insertCartSchema,
+  cartItemSchema,
+} from "@/lib/validators";
 import { z } from "zod";
 
 export type Product = z.infer<typeof insertProductSchema> & {
@@ -6,3 +10,6 @@ export type Product = z.infer<typeof insertProductSchema> & {
   rating: string;
   createdAt: Date;
 };
+
+export type Cart = z.infer<typeof insertCartSchema>;
+export type CartItem = z.infer<typeof cartItemSchema>;
