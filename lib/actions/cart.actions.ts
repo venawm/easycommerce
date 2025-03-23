991f3b34c3a360b4e0dbd146b78c3007b6ef3905
venawm <pandesamir3@gmail.com>
2025-03-23 23:03:25 +0545

    feat(cart):add session id validation to the cart

--- lib/actions/cart.actions.ts
+++ lib/actions/cart.actions.ts
@@ -1,10 +1,72 @@
 "use server";
 
+import { cookies } from "next/headers";
 import { CartItem } from "@/types";
+import { converToObject, formatError } from "../utils";
+import { auth } from "@/auth";
+import { prisma } from "@/db/prisma";
+import { cartItemSchema } from "../validators";
 
 export async function addItemToCart(data: CartItem) {
-  return {
-    success: true,
-    message: "Item added to cart sucessfully",
-  };
+  try {
+    // Check for the cart cookie
+    const sessionCartId = (await cookies()).get("sessionCartId")?.value;
+
+    if (!sessionCartId) throw new Error("Cart not found please try again");
+
+    const session = await auth();
+
+    const userId = session?.user?.id ? (session.user.id as string) : undefined;
+
+    const cart = await getMyCart();
+
+    // Parse and validate item
+    const item = cartItemSchema.parse(data);
+
+    const product = await prisma.product.findFirst({
+      where: { id: item.productId },
+    });
+
+    console.log(product);
+
+    return {
+      success: true,
+      message: "Item added to cart sucessfully",
+    };
+  } catch (error) {
+    return {
+      success: false,
+      message: formatError(error),
+    };
+  }
+}
+
+export async function getMyCart() {
+  // Check for the cart cookie
+  const sessionCartId = (await cookies()).get("sessionCartId")?.value;
+
+  if (!sessionCartId) throw new Error("Cart not found please try again");
+
+  const session = await auth();
+
+  const userId = session?.user?.id ? (session.user.id as string) : undefined;
+
+  // Get user cart from database
+
+  const cart = await prisma.cart.findFirst({
+    where: userId ? { userId: userId } : { sessionCartId: sessionCartId },
+  });
+
+  if (!cart) return undefined;
+
+  // convert decimals and return
+
+  return converToObject({
+    ...cart,
+    items: cart.items as CartItem[],
+    itemsPrice: cart.itemsPrice.toString(),
+    totalPrice: cart.totalPrice.toString(),
+    shippingPrice: cart.shippingPrice.toString(),
+    taxPrice: cart.taxPrice.toString(),
+  });
 }
